1a5037860d956755e119a5a4a84a4118725a10df
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-04-16 19:08:50 -0400

    Added post request

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// Package to generate unique ids
+const { v4: uuidv4 } = require('uuid');
 // Require the JSON file and assign it to a variable called `termData`
 const notesData = require('./db/db.json');
-
-// Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001
 
@@ -24,11 +23,37 @@ app.get('/notes', (req, res) => {
   console.info(`${req.method} request received to get notes`);
 });
 
+// Get request for note data from db.json
 app.get('/api/notes', (req, res) => {
     res.json(notesData)
   });
 
-app.post('/api/notes')
+// POST request to add a new note
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+  
+    // Generate a unique id for the new note using the uuid package
+    const newId = uuidv4();
+    const newNote = {
+      id: newId,
+      title,
+      text,
+    };
+  
+    // Add the new note to the notesData array
+    notesData.push(newNote);
+  
+    // Write the updated notesData array back to the db.json file
+    fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error saving note');
+      } else {
+        // Return the new note as the response to the client
+        res.json(newNote);
+      }
+    });
+  });
 
 // Wildcard request for index html
 app.get('*', (req, res) =>
